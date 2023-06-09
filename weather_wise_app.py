f85e496cc9609576ffca42cd57893dcc1d7b9604
moinkhan3012@gmail.com <Moin Khan>
2023-06-09 01:59:26 -0400

    added weather_wise_app.py

---
+++ weather_wise_app.py
@@ -0,0 +1,63 @@
+import os
+import streamlit as st
+
+from langchain.llms import OpenAI
+
+from langchain import LLMChain
+from langchain.agents import load_tools,ZeroShotAgent, AgentExecutor
+
+from langchain.memory import ConversationBufferMemory
+
+from dotenv import load_dotenv
+load_dotenv()
+
+st.title(':red[LangChain Weather Predictor]')
+
+def initialize():
+
+    llm= OpenAI(temperature =0, model_name = 'gpt-3.5-turbo')
+
+    tools = load_tools(["openweathermap-api",'google-search'], llm)
+
+
+    prefix = """Yor are a weather activity application, and you are trained to return the weather of the location. If ask any further subsequent question for the location, you need to answer the question considering the location given by user as a input. To better utitlize the tools, you should save the country of the location. Whenever using the OpenWeatherMap tool, you should pass the actuion input in this form, City/State/location, Country name, eg. Texas,US. You have access to the following tools"""
+    suffix = """Begin!"
+    {chat_history}
+    Question: {input}
+    {agent_scratchpad}"""
+
+    prompt = ZeroShotAgent.create_prompt(
+        tools, 
+        prefix=prefix, 
+        suffix=suffix, 
+        input_variables=["input", "chat_history", "agent_scratchpad"]
+    )
+    memory = ConversationBufferMemory(memory_key="chat_history")
+
+    llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+    agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+
+    return agent_chain
+
+prompt = st.text_input('Ask your questions here!!!')
+
+
+if st.button(":red[Reset]"):
+    # print('button initialize')
+    st.session_state.agent_chain = initialize()
+    prompt = ''
+    
+
+if 'agent_chain' not in st.session_state:
+    st.session_state.agent_chain = initialize()
+
+
+if prompt:
+    # print(prompt)
+    try:
+        st.write(st.session_state.agent_chain.run(input=prompt))
+        
+    except Exception as err:
+        st.write(f":red[{type(err).__name__}\:{err}]")
+        
