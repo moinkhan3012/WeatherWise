ed9334e82535d7a964cd3b2275cd5f5acd68cdac
moinkhan3012@gmail.com <Moin Khan>
2023-06-12 16:38:38 -0400

    Added support for openaikey by user

--- weather_wise_app.py
+++ weather_wise_app.py
@@ -7,14 +7,20 @@ from langchain import LLMChain
 from langchain.agents import load_tools,ZeroShotAgent, AgentExecutor
 
 from langchain.memory import ConversationBufferMemory
-
+from pyowm.commons.exceptions import NotFoundError
 from dotenv import load_dotenv
 load_dotenv()
 
-st.title(':red[LangChain Weather Predictor]')
+def verifyOpenAIKey(key:str):
+    if not (key.startswith('sk-') and len(key)==51):
+        st.warning('OpenAI Key is not correct.', icon='⚠')
+        
+        return False
+
+    return True
+
 
 def initialize():
-
     llm= OpenAI(temperature =0, model_name = 'gpt-3.5-turbo')
 
     tools = load_tools(["openweathermap-api",'google-search'], llm)
@@ -40,24 +46,52 @@ def initialize():
 
     return agent_chain
 
-prompt = st.text_input('Ask your questions here!!!')
+
+def main():
+    st.sidebar.title('OpenAI API Key')
+    openai_api_key = st.sidebar.text_input('Enter OpenAI API Key here or [get one here](https://platform.openai.com/account/api-keys)')
+    if 'openai_api_key' not in st.session_state:
+        print(openai_api_key)
+        if verifyOpenAIKey(openai_api_key):    
+            if openai_api_key:
+                st.session_state.openai_api_key = openai_api_key
+                os.environ['OPENAI_API_KEY'] = openai_api_key
+        else:
+            return
+    else: 
+        os.environ['OPENAI_API_KEY'] = st.session_state.openai_api_key
+
+    prompt = st.text_input('Ask your questions here!!!')
 
 
-if st.button(":red[Reset]"):
-    # print('button initialize')
-    st.session_state.agent_chain = initialize()
-    prompt = ''
+    if st.button("Reset", type="primary"):
+        # print('button initialize')
+        st.session_state.agent_chain = initialize()
+        prompt = ''
+        
+
+    if 'agent_chain' not in st.session_state:
+        st.session_state.agent_chain = initialize()
+
+
+    if prompt:
+        # print(prompt)
+        try:
+            st.info(st.session_state.agent_chain.run(input=prompt))
+            
+        except NotFoundError as err:
+            st.error(f":red[{type(err).__name__}\:{err}  \nHint: City name not found in the prompt]")
+
+        except Exception as err:
+            
+            st.error(f":red[{type(err).__name__}\:{err}]")
+
+        with st.expander('Conversation History'):
+            st.info(st.session_state.agent_chain.memory.buffer.replace('\n','  \n'))
+
+  
+if __name__ == "__main__":
+    st.title(':red[LangChain Weather Predictor]')
+    main()
     
-
-if 'agent_chain' not in st.session_state:
-    st.session_state.agent_chain = initialize()
-
-
-if prompt:
-    # print(prompt)
-    try:
-        st.write(st.session_state.agent_chain.run(input=prompt))
-        
-    except Exception as err:
-        st.write(f":red[{type(err).__name__}\:{err}]")
-        
+            
