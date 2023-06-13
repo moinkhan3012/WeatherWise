7b8e4f85bd44ac4f4cc5c4681c9bb72109fc0ec3
moinkhan3012@gmail.com <Moin Khan>
2023-06-13 13:18:19 -0400

    refactored and formatting

--- weather_wise_app.py
+++ weather_wise_app.py
@@ -2,29 +2,25 @@ import os
 import streamlit as st
 
 from langchain.llms import OpenAI
-
 from langchain import LLMChain
 from langchain.agents import load_tools,ZeroShotAgent, AgentExecutor
-
 from langchain.memory import ConversationBufferMemory
+
 from pyowm.commons.exceptions import NotFoundError
+
 from dotenv import load_dotenv
 load_dotenv()
 
 def verifyOpenAIKey(key:str):
     if not (key.startswith('sk-') and len(key)==51):
         st.warning('OpenAI Key is not correct.', icon='⚠')
-        
         return False
-
     return True
-
 
 def initialize():
     llm= OpenAI(temperature =0, model_name = 'gpt-3.5-turbo')
 
     tools = load_tools(["openweathermap-api",'google-search'], llm)
-
 
     prefix = """Yor are a weather activity application, and you are trained to return the weather of the location. If ask any further subsequent question for the location, you need to answer the question considering the location given by user as a input. To better utitlize the tools, you should save the country of the location. Whenever using the OpenWeatherMap tool, you should pass the actuion input in this form, City/State/location, Country name, eg. Texas,US. You have access to the following tools"""
     suffix = """Begin!"
@@ -46,12 +42,10 @@ def initialize():
 
     return agent_chain
 
-
 def main():
     st.sidebar.title('OpenAI API Key')
     openai_api_key = st.sidebar.text_input('Enter OpenAI API Key here or [get one here](https://platform.openai.com/account/api-keys)')
     if 'openai_api_key' not in st.session_state:
-        print(openai_api_key)
         if verifyOpenAIKey(openai_api_key):    
             if openai_api_key:
                 st.session_state.openai_api_key = openai_api_key
@@ -65,17 +59,14 @@ def main():
 
 
     if st.button("Reset", type="primary"):
-        # print('button initialize')
         st.session_state.agent_chain = initialize()
         prompt = ''
-        
 
     if 'agent_chain' not in st.session_state:
         st.session_state.agent_chain = initialize()
 
 
     if prompt:
-        # print(prompt)
         try:
             st.info(st.session_state.agent_chain.run(input=prompt))
             
@@ -83,7 +74,6 @@ def main():
             st.error(f":red[{type(err).__name__}\:{err}  \nHint: City name not found in the prompt]")
 
         except Exception as err:
-            
             st.error(f":red[{type(err).__name__}\:{err}]")
 
         with st.expander('Conversation History'):
@@ -92,10 +82,8 @@ def main():
   
 if __name__ == "__main__":
     st.set_page_config(
-        page_title=":red[Weather Wise]",
+        page_title=":Weather Wise",
         page_icon="robot_face",
         layout="wide")
     st.title(':red[Weather Wise]')
     main()
-    
-            
